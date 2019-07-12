b4bedf218dec01f18f771642dc8ba9234b45e90e
黄瑞 <13618641207@163.com>
2019-07-12 15:22:32 +0800

    Initial Commit

---
+++ DocumentPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DocumentPlayer
+//
+//  Created by 黄瑞 on 2019/7/12.
+//  Copyright © 2019 黄瑞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DocumentPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DocumentPlayer
+//
+//  Created by 黄瑞 on 2019/7/12.
+//  Copyright © 2019 黄瑞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
